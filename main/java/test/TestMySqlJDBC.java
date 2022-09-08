26b97276f3dd2c6f8b803eebf4238cd7731439d9
Ivan Kaczek <ivan.kaczek@gmail.com>
2022-09-08 15:31:08 -0300

    first commit, 1st connection

---
+++ main/java/test/TestMySqlJDBC.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestMySqlJDBC {
+    public static void main(String[] args) throws SQLException {
+        // Escribimos cadena de conexión para conectarnos con MySql
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(url,"root","root");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            while(resultado.next()){
+                System.out.println("Id Persona: " + resultado.getInt("id_persona"));
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch(SQLException ex){
+            ex.printStackTrace(System.out);
+        }
+        
+        
+        
+    }
+    
+}
